66564d139d10c3ddfbbe09de433d913ba6235cd5
BrandonSalimTheHuman <114371928+BrandonSalimTheHuman@users.noreply.github.com>
2024-06-20 04:40:13 +0700

    Update HarborScanPage.jsx

--- src/Harbor/components/HarborScanPage.jsx
+++ src/Harbor/components/HarborScanPage.jsx
@@ -1,17 +1,18 @@
-import "../styles/HarborScanPage.css";
-import Header2 from "../../Assets/centra-main-header.svg";
-import arrow from "../../Assets/Arrow.svg";
-import bell from "../../Assets/BellTemp.svg";
-import gallery from "../../Assets/gallery.svg";
-import flash from "../../Assets/flash.svg";
-import QrScanner from "react-qr-scanner";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import ProfilePictureMini from "../../Assets/modules/ProfilePictureMini.jsx";
-import { Height } from "@mui/icons-material";
+import '../styles/HarborScanPage.css';
+import Header2 from '../../Assets/centra-main-header.svg';
+import arrow from '../../Assets/Arrow.svg';
+import bell from '../../Assets/BellTemp.svg';
+import gallery from '../../Assets/gallery.svg';
+import flash from '../../Assets/flash.svg';
+import QrScanner from 'react-qr-scanner';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ProfilePictureMini from '../../Assets/modules/ProfilePictureMini.jsx';
+import { Button, TextField } from '@mui/material';
 
 const ScanPage = () => {
-  const [data, setData] = useState("No result");
+  const [data, setData] = useState('No result');
+  const [id, setId] = useState('');
 
   const navigate = useNavigate();
 
@@ -25,54 +26,77 @@ const ScanPage = () => {
     console.error(err);
   };
 
+  const handleInputtedId = () => {
+    localStorage.setItem('id', id);
+    navigate('/harbor/detail');
+  };
+
   return (
-    <div className="harbor-page-container">
-      <div className="harbor-top-section">
-        <img className="harbor-main-header-extra" src={Header2} alt="header" />
-        <div className="harbor-top-row">
+    <div className='harbor-page-container'>
+      <div className='harbor-top-section'>
+        <img className='harbor-main-header-extra' src={Header2} alt='header' />
+        <div className='harbor-top-row'>
           <img
-            className="arrow"
+            className='arrow'
             src={arrow}
-            alt="Arrow"
+            alt='Arrow'
             onClick={() => {
-              navigate("/harbor/homepage");
+              navigate('/harbor/homepage');
             }}
           />
-          <h3 className="harbor-other-title-extra">Scan Barcode</h3>
-          <div className="right-icons">
+          <h3 className='harbor-other-title-extra'>Scan Barcode</h3>
+          <div className='right-icons'>
             <img
-              className="bell"
+              className='bell'
               src={bell}
-              alt="Bell"
+              alt='Bell'
               onClick={() => {
-                navigate("/harbor/notifications");
+                navigate('/harbor/notifications');
               }}
             />
             <ProfilePictureMini />
           </div>
         </div>
       </div>
-      <div className="scan-framebox">
-        <div className="scan-message1">
+      <div className='scan-framebox'>
+        <div className='scan-message1'>
           Place barcode inside the frame to scan
         </div>
-        <div className="scan-picture">
+        <div className='scan-picture'>
           <QrScanner
             delay={300}
             onScan={handleScan}
             onError={handleError}
-            style={{ width: "95%", height: "95%" }}
+            style={{ width: '95%', height: '95%' }}
           />
         </div>
-        <div className="scan-message2">Scanning Barcode: {data}</div>
-        <div className="picture-icons">
+        <div className='scan-message2'>Scanning Barcode: {data}</div>
+        <div className='picture-icons'>
           <img
             src={gallery}
-            alt="gallery"
-            onClick={() => console.log("gallery")}
+            alt='gallery'
+            onClick={() => console.log('gallery')}
           />
-          <img src={flash} alt="flash" onClick={() => console.log("flash")} />
+          <img src={flash} alt='flash' onClick={() => console.log('flash')} />
         </div>
+      </div>
+      <div className='area-below-scanning'>
+        <TextField
+          label='Alternatively, write id here'
+          variant='outlined'
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <Button
+          onClick={handleInputtedId}
+          color='success'
+          variant='contained'
+          sx={{ textTransform: 'none' }}
+        >
+          Confirm
+        </Button>
       </div>
     </div>
   );
